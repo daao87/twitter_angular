c432a26d571444f02cd5f2680d9b61809a87cfdc
Diego <diego@ubuntuvm.(none)>
2015-03-17 15:28:52 +0100

    change app.js back (previous CORS setup not needed)

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -1,10 +1,7 @@
 angular.module('AngularRails', [
         'ngRoute',
         'templates'
-    ]).config(['$httpProvider', '$routeProvider', '$locationProvider', function ($httpProvider, $routeProvider, $locationProvider) {
-        
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    ]).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         
         $routeProvider.when('/tweets', {templateUrl: 'tweets.html', controller: 'TweetsCtrl'});
         $routeProvider.otherwise({templateUrl: 'home.html', controller: 'HomeCtrl'});
